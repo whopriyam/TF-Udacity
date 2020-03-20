27cf628ec75c85998666560d0f77483072974d74
whopriyam <priyambasu16@gmail.com>
2020-03-20 23:37:15 +0530

    Add existing file

---
+++ python2.py
@@ -0,0 +1,13 @@
+print ("Iterate over the items")
+for i in range(2):
+	print (i)
+
+print ("Iterate over an actual list")
+for i in [0,1]:
+	print (i)
+
+print ("While works")
+i=0
+while i<2:
+	print i
+	i=i+1
